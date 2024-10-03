fe269fef1b37bd255bcfb088f379c7642345db42
LeonardoRFragoso <LeonardoRFragoso@gmail.com>
2024-10-03 18:43:44 -0300

    Ajustando ContactSection da HomePage do projeto

--- src/pages/HomePage/TestimonialsSection/TestimonialsSection.js
+++ src/pages/HomePage/TestimonialsSection/TestimonialsSection.js
@@ -7,21 +7,24 @@ import avatar3 from '../../../assets/img/avatar3.jpg'; // Caminho para a imagem 
 
 const TestimonialsSection = () => {
   return (
-    <section className={styles.testimonialsSection}>
-      <h2 className={styles.testimonialsTitle}>O que nossos clientes dizem</h2> {/* Aplicando a classe CSS correta */}
+    <section className={`${styles.testimonialsSection} ${styles.sectionWithNeonBorder}`}>
+      <h2 className={styles.testimonialsTitle}>O que nossos clientes dizem</h2> {/* Título da seção com classe CSS correta */}
       <div className={styles.testimonialsContainer}>
+        {/* Testimonial 1 */}
         <div className={styles.testimonialItem}>
           <img src={avatar1} alt="Cliente 1" className={styles.testimonialAvatar} />
           <blockquote>
             "A Pyscript.tech transformou nossa presença online! O site ficou incrível!" - Cliente Satisfeito
           </blockquote>
         </div>
+        {/* Testimonial 2 */}
         <div className={styles.testimonialItem}>
           <img src={avatar2} alt="Cliente 2" className={styles.testimonialAvatar} />
           <blockquote>
             "Os bots criados pela Pyscript.tech otimizaram nossos processos internos, uma verdadeira revolução!" - Outro Cliente
           </blockquote>
         </div>
+        {/* Testimonial 3 */}
         <div className={styles.testimonialItem}>
           <img src={avatar3} alt="Cliente 3" className={styles.testimonialAvatar} />
           <blockquote>
