f476b28f367d46e1ee307ee1084b6bb2b305adf3
LeonardoRFragoso <LeonardoRFragoso@gmail.com>
2024-10-03 08:56:16 -0300

    Adicionando JS e CSS da página de Contato e ajustando página de Projetos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/navbar'; // Verifique se o caminho e nome do arquivo estão corretos
+import Navbar from './components/Navbar/navbar'; // Certifique-se que o caminho e nome do arquivo estão corretos
 import HomePage from './pages/HomePage/HomePage'; // Certifique-se que o caminho e nome do arquivo estão corretos
-import ServicesPage from './pages/Services/ServicesPage'; // Verifique se o caminho e nome do arquivo estão corretos
+import ServicesPage from './pages/Services/ServicesPage'; // Certifique-se que o caminho e nome do arquivo estão corretos
 import ProjectsPage from './pages/ProjectsPage/ProjectsPage'; // Certifique-se que o caminho e nome do arquivo estão corretos
-import Footer from './components/Footer/footer'; // Verifique se o caminho e nome do arquivo estão corretos e o arquivo existe
+import ContactPage from './pages/ContactPage/ContactPage'; // Nova página de contato
+import Footer from './components/Footer/footer'; // Certifique-se que o caminho e nome do arquivo estão corretos e o arquivo existe
 
 // Importação dos estilos do carrossel
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
@@ -21,7 +22,7 @@ const App = () => {
             <Route path="/" element={<HomePage />} /> {/* Página inicial */}
             <Route path="/services" element={<ServicesPage />} /> {/* Página de serviços */}
             <Route path="/projects" element={<ProjectsPage />} /> {/* Página de projetos */}
-            {/* Adicione outras rotas conforme cria os arquivos correspondentes */}
+            <Route path="/contact" element={<ContactPage />} /> {/* Nova página de contato */}
           </Routes>
         </main>
         <Footer /> {/* Rodapé da página */}
